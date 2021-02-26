ed66adc5ef458dbabb0f5b7623155171ac4f67bf
Francisco J Ibarrola <fibarrola@sinc.unl.edu.ar>
2021-02-26 12:52:17 -0300

    added function to filter by location

--- helpers/mascotaService.js
+++ helpers/mascotaService.js
@@ -36,6 +36,24 @@ async function actualizarArchivo(file, perroId, token) {
     return false;
   }
 }
+ 
+
+function filtrar(mascotas, user, dist2) {
+  // definir una nueva variable mascotasFiltered 
+	var masctoasFiltered 
+	var userLat = user.location.latitude
+	var userLon = user.location.longitude 
+  // para cada mascota, chequeo si esta a menos de sqrt(dist) de distancia y en tal caso la agrego a mascotasFiltered 
+	for (let ind = 0; ind < mascotas.lenght; ind++) {
+		var petLat = mascotas[ind].location.latitude
+		var petLon = mascotas[ind].location.longitude
+		if( (petLat-userLat)**2+(petLon-userLon)**2 < dist2 ) {
+			mascotasFiltered.append(mascotas[ind])	
+			}
+		}
+	return mascotasFiltered
+	}
+  
 
 async function getMascotas(){
   return await fetch(`https://mascotass.herokuapp.com/api/mascotas`,{
@@ -47,6 +65,7 @@ async function getMascotas(){
     }
   })
     .then((response) => response.json())
+    // .then(({ mascotas }) => filtrar(mascotas, user, 100)
     .then(({ mascotas }) => mascotas.reverse())
     .catch((error) => console.error(error));
 }
